ce2b96b52e2ea28fdd5d2fc4adf55d5c033a971a
Farras Hafizhudin <103520002+farrasjay@users.noreply.github.com>
2022-10-25 01:04:45 +0700

    initiate authentication and landing page

--- uhealths/urls.py
+++ uhealths/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'uhealths'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.landingpage, name='landingpage'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout')
 ]
--- uhealths/views.py
+++ uhealths/views.py
@@ -1,5 +1,47 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import datetime, json
+from django.urls import reverse
+from django.core import serializers
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-def index(request):
-    return HttpResponse("Hello BO9!")
+@login_required(login_url='/uhealths/login/')
+def landingpage(request):
+    return render(request, 'landingpage.html')
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun berhasil teregister!')
+            return redirect('uhealths:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("uhealths:landingpage"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('uhealths:login'))
+    response.delete_cookie('last_login')
+    return response
